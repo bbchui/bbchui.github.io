121d5bc8d062698dbb45bc0c1b246945b18826ec
bbchui <%= GitConfig.user_email %>
2017-05-30 11:32:24 -0700

    got bamboo to render and move

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var webpack = require("webpack");
 
 module.exports = {
   context: __dirname,
-  entry: "./lib/game.js",
+  entry: "./lib/pandakoid.js",
   output: {
     path: path.resolve(__dirname, 'lib'),
     filename: "bundle.js"
